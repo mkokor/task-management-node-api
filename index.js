79e2b578893804b42793143f891a087a208572d1
mkokor <matijakokorr@gmail.com>
2023-07-25 16:55:31 +0200

    tasks router configured

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Task Manager API");
+const express = require("express");
+const tasksRouter = require("./routes/tasks.route");
+
+const app = express();
+const PORT = 3000;
+
+
+app.use("/api/tasks", tasksRouter);
+
+
+app.listen(PORT, () => {
+  console.log(`Task manager API is listening on port ${PORT}...`);
+});
