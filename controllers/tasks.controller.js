6ef14942c99b8c04359971ce91feea9c6259b3a3
mkokor <matijakokorr@gmail.com>
2023-07-27 21:41:08 +0200

    custom promise error middleware replaced with npm module

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,30 +1,29 @@
 const taskService = require("../services/tasks.service");
-const { asyncWrapper } = require("../middleware/async-wrapper");
 
-const getAllTasks = asyncWrapper(async (req, res) => {
+const getAllTasks = async (req, res) => {
   const result = await taskService.getAllTasks();
   res.status(200).json(result);
-});
+};
 
-const createTask = asyncWrapper(async (req, res) => {
+const createTask = async (req, res) => {
   const result = await taskService.createTask(req.body);
   res.status(201).json(result);
-});
+};
 
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = async (req, res) => {
   const result = await taskService.getTaskById(req.params.id);
   res.status(200).json(result);
-});
+};
 
-const updateTask = asyncWrapper(async (req, res) => {
+const updateTask = async (req, res) => {
   const result = await taskService.updateTask(req.params.id, req.body);
   res.status(200).json(result);
-});
+};
 
-const deleteTask = asyncWrapper(async (req, res) => {
+const deleteTask = async (req, res) => {
   await taskService.deleteTask(req.params.id);
   res.status(200).json({ message: "Task successfully deleted." });
-});
+};
 
 module.exports = {
   getAllTasks,
